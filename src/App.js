d6904e97de4353a3903e57aadae856ab1cdf2bcd
aslampangestu <aslampangestu@gmail.com>
2019-07-10 12:38:04 +0700

    counter withut hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { useState } from 'react'
 
-function App() {
-  const [activated, setActivated] = useState(false)
-  const btnText = activated ? 'ON' : 'OFF'
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+    this.handleClickIncrease = this.handleClickIncrease.bind(this)
+    this.handleClickDecrease = this.handleClickDecrease.bind(this)
+  }
 
-  const onClick = () => setActivated(!activated)
-  return <button onClick={onClick}>{btnText}</button>
+  handleClickIncrease() {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }))
+  }
+
+  handleClickDecrease() {
+    this.setState(prevState => ({
+      count: prevState.count - 1
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClickIncrease}>Increase</button>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClickDecrease}>Decrease</button>
+      </div>
+    )
+  }
 }
 
 export default App
