3668e3219a22b8c573a79b482b97160fb267c92f
aslampangestu <aslampangestu@gmail.com>
2019-07-10 11:24:49 +0700

    review pros & statlest component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
+function Box(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Statelsess Component</h1>
+      <h1>{props.text}</h1>
+      <h2>{props.subtitle}</h2>
     </div>
-  );
+  )
+}
+function App() {
+  return <Box text='Testing' subtitle='Test Subtitle' />
 }
 
-export default App;
+export default App
