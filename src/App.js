bf440329304316b2eca3ec65389a4c96bf706a92
aslampangestu <aslampangestu@gmail.com>
2019-07-10 12:05:45 +0700

    simple introduce usestate

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class Button extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      activated: false
-    }
-  }
+function App() {
+  const [activated, setActivated] = useState(false)
+  const btnText = activated ? 'ON' : 'OFF'
 
-  handleActiveChange = () => {
-    // this.setState({ activated: !this.state.activated })
-    this.setState(prevState => ({
-      activated: !prevState.activated
-    }))
-  }
-
-  render() {
-    const btnText = this.state.activated
-      ? this.props.activeText
-      : this.props.inactiveText
-    return <button onClick={this.handleActiveChange}>{btnText}</button>
-  }
-}
-class App extends Component {
-  render() {
-    return <Button activeText='On' inactiveText='Off' />
-  }
+  const onClick = () => setActivated(!activated)
+  return <button onClick={onClick}>{btnText}</button>
 }
 
 export default App
