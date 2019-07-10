7a44a23e1e3ea145235bcbdb5304038fe00d8d82
aslampangestu <aslampangestu@gmail.com>
2019-07-10 12:43:02 +0700

    counter with hook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,57 @@
 import React, { useState } from 'react'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 0
-    }
-    this.handleClickIncrease = this.handleClickIncrease.bind(this)
-    this.handleClickDecrease = this.handleClickDecrease.bind(this)
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       count: 0
+//     }
+//     this.handleClickIncrease = this.handleClickIncrease.bind(this)
+//     this.handleClickDecrease = this.handleClickDecrease.bind(this)
+//   }
 
-  handleClickIncrease() {
-    this.setState(prevState => ({
-      count: prevState.count + 1
-    }))
-  }
+//   handleClickIncrease() {
+//     this.setState(prevState => ({
+//       count: prevState.count + 1
+//     }))
+//   }
 
-  handleClickDecrease() {
-    this.setState(prevState => ({
-      count: prevState.count - 1
-    }))
-  }
+//   handleClickDecrease() {
+//     this.setState(prevState => ({
+//       count: prevState.count - 1
+//     }))
+//   }
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClickIncrease}>Increase</button>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClickDecrease}>Decrease</button>
-      </div>
-    )
-  }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClickIncrease}>Increase</button>
+//         <h1>{this.state.count}</h1>
+//         <button onClick={this.handleClickDecrease}>Decrease</button>
+//       </div>
+//     )
+//   }
+// }
+
+function App() {
+  const [count, setCount] = useState(0)
+  // const handleClickIncrease = () => setCount(count + 1)
+  // const handleClickDecrease = () => setCount(count - 1)
+  const handleClickIncrease = () =>
+    setCount(prevCount => {
+      return prevCount + 1
+    })
+  const handleClickDecrease = () =>
+    setCount(prevCount => {
+      return prevCount - 1
+    })
+  return (
+    <div>
+      <button onClick={handleClickIncrease}>Increase</button>
+      <h1>{count}</h1>
+      <button onClick={handleClickDecrease}>Decrease</button>
+    </div>
+  )
 }
 
 export default App
