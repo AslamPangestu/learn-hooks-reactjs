ef0c4ca5775195f22a2a7ff3addf3fbdb9be8c3a
aslampangestu <aslampangestu@gmail.com>
2019-07-22 13:28:05 +0700

    usecontext

--- src/App.js
+++ src/App.js
@@ -1,52 +1,35 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState, useContext } from 'react'
 
 const NameContext = createContext()
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: 'Budi'
-    }
-  }
-
-  render() {
-    return (
-      <NameContext.Provider value={this.state.name}>
-        <Child />
-      </NameContext.Provider>
-    )
-  }
+function App() {
+  const [name, setName] = useState('Budi')
+  return (
+    <NameContext.Provider value={name}>
+      <Child />
+    </NameContext.Provider>
+  )
 }
 
-class Child extends React.Component {
-  render() {
-    return (
-      <section className='child'>
-        <GrandChild />
-      </section>
-    )
-  }
+function Child() {
+  return (
+    <section className='child'>
+      <GrandChild />
+    </section>
+  )
 }
 
-class GrandChild extends React.Component {
-  render() {
-    return (
-      <div className='child'>
-        <Button />
-      </div>
-    )
-  }
+function GrandChild() {
+  return (
+    <div className='child'>
+      <Button />
+    </div>
+  )
 }
 
-class Button extends React.Component {
-  render() {
-    return (
-      <NameContext.Consumer>
-        {name => <button>{name}</button>}
-      </NameContext.Consumer>
-    )
-  }
+function Button() {
+  const name = useContext(NameContext)
+  return <button>{name}</button>
 }
 
 export default App
