b8ec3be5c852ef51768ea800ccd0d9afe0e56882
aslampangestu <aslampangestu@gmail.com>
2019-07-22 12:16:32 +0700

    simple useeffect

--- src/App.js
+++ src/App.js
@@ -1,47 +1,26 @@
 import React, { useState, useEffect } from 'react'
 
-function Counter() {
-  const [count, setCount] = useState(0)
-  const [color, setColor] = useState('salmon')
-  function handleIncrease() {
-    setCount(prevCount => {
-      return prevCount + 1
-    })
-  }
+function App() {
+  const [userText, setUserText] = useState('')
 
-  const handleDecrease = () =>
-    setCount(prevCount => {
-      return prevCount - 1
-    })
-
-  function handleColorChange() {
-    const nextColor = color === 'salmon' ? 'gold' : 'salmon'
-    setColor(nextColor)
+  function handleUserKeyPress(event) {
+    const { key, keyCode } = event
+    if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
+      setUserText(`${userText}${key}`)
+    }
   }
 
   useEffect(() => {
-    console.log(`I'm Inside useEffect func. The current count = ${count}`)
-
+    window.addEventListener('keydown', handleUserKeyPress)
     return () => {
-      console.log(`Remove anytihng, last count = ${count}`)
+      window.removeEventListener('keydown', handleUserKeyPress)
     }
-  }, [count])
+  })
 
   return (
     <div>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button>
-      <button onClick={handleColorChange}>Change color</button>
-      <h1 style={{ color: color }}>{count}</h1>
-    </div>
-  )
-}
-function App() {
-  const [visibel, setVisibel] = useState(false)
-  return (
-    <div>
-      <button onClick={() => setVisibel(!visibel)}>Show/Hide Component</button>
-      {visibel && <Counter />}
+      <h1>Feel free to type</h1>
+      <blockquote>{userText}</blockquote>
     </div>
   )
 }
