05e02d2b514ef4b39e9031a6a8f343b3a6625cfa
aslampangestu <aslampangestu@gmail.com>
2019-07-23 09:48:35 +0700

    custom hook - implementation 1

--- src/App.js
+++ src/App.js
@@ -1,55 +1,39 @@
 import React, { useState } from 'react'
 
 function App() {
+  const { value: name, changeHandler: changeName } = useInputText()
+  const { value: surename, changeHandler: changeSurename } = useInputText()
+  const { value: age, changeHandler: changeAge } = useInputText()
   return (
-    <div>
-      <Display start={10} />
-      <Display start={20} />
-      <FancyDisplay start={30} />
-    </div>
+    <form>
+      <input
+        type='text'
+        placeholder='Name'
+        value={name}
+        onChange={changeName}
+      />
+      <input
+        type='text'
+        placeholder='Surename'
+        value={surename}
+        onChange={changeSurename}
+      />
+      <input type='number' placeholder='Age' value={age} onChange={changeAge} />
+    </form>
   )
 }
 
-function useCounter(startingValue) {
-  const [count, setCount] = useState(startingValue)
-  function handleIncrease() {
-    setCount(prevCount => {
-      return prevCount + 1
-    })
-  }
-  function handleDecrease() {
-    setCount(prevCount => {
-      return prevCount - 1
-    })
+function useInputText() {
+  const [value, setValue] = useState('')
+
+  function changeHandler(event) {
+    setValue(event.target.value)
   }
 
   return {
-    count,
-    handleIncrease,
-    handleDecrease
+    value,
+    changeHandler
   }
-}
-
-function Display(props) {
-  const { count, handleIncrease, handleDecrease } = useCounter(props.start)
-  return (
-    <div>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button>
-      <h1>{count}</h1>
-    </div>
-  )
-}
-
-function FancyDisplay(props) {
-  const { count, handleIncrease, handleDecrease } = useCounter(props.start)
-  return (
-    <section>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button>
-      <h2>{count}</h2>
-    </section>
-  )
 }
 
 export default App
